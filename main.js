3502400820709eae59d022d24b6e94ccf8cdd6a6
david dassau <todieahero@hotmail.com>
2017-07-27 12:41:30 -0500

    added JS functionality to blog page

---
+++ main.js
@@ -0,0 +1,93 @@
+// Main JS File
+
+
+// blog.html
+
+// Declare empty array
+var allPosts = [];
+
+// Declare all post variables
+var post1 = {
+	postTitle: "Catalyst",
+	postDate: "July 8, 2017",
+	postBody: "<p>Back to school, back to school, to prove to myself that I&rsquo;m not a fool.</p><p>I was pretty nervous about going back to school, for several reasons. First of all, I never performed exceptionally well academically. In laymen&rsquo;s terms, I&rsquo;m kind of stupid. Well, not stupid per se, but it does take me a little while to catch on. Secondly, I was going back to school for web and software development, which is a field that I&rsquo;ve always struggled with. I love working with code, but I always found myself quickly discouraged. I would constantly hit road blocks where I didn&rsquo;t really understand what I was looking at, or what was being taught to me. But on the other hand, school WAS the silver lining. Up until now, most of my coding education has been self-taught, so I was also excited to be in an actual classroom setting, with physical teachers, surrounded by fellow classmates with more than likely the same questions I have.</p><p>The first day of class was on Saturday, July 8th 2017. Most of the day served as an orientation to how things were going to go for the next 12 months, and what we can expect to happen. My feelings ranged from happiness, to fear, to elation, to despair, to excitement. And we hadn&rsquo;t even gotten into any of the material yet!</p><p>Putting aside all of my personal feelings about going back to school for development, I&rsquo;d love to touch on the diversity of my classmates. This was something I was curious to learn. It&rsquo;s no big secret that the tech industry isn&rsquo;t really just for Asians, Indians, and weird looking white guys. It&rsquo;s for everyone! So I was very excited to walk into class on the first day, and see that I had struck the diversity jackpot. In fact, if I hadn&rsquo;t have known that this was a class/school for web and software development, I would have thought it was your typical general education class down at Nashville State Community College. There were folks young and old, black and white, male and female. It was genuinely really cool to know I would be a part of this learning environment.</p><p>I left school that day with the same feelings and attitude as I did when I walked in: nervous, but excited. I&rsquo;ll be happy and content if those feelings stay the same.</p>"
+};
+
+var post2 = {
+	postTitle: "It's Ok To Cry",
+	postDate: "July 10, 2017",
+	postBody: "<p>“What the hell have I gotten myself into?”</p><p>Those were the words that were ringing through my head at the end of my first “official” day at NSS. For the entirety of the evening, we sat and watched as the Evening Cohort 5 class presented their capstone projects. Much like the first day on campus just two days earlier, I was experiencing many different emotions, mostly fear and dread. For their capstone projects, the students had to take all of what they’d learned the past 5 and a half months, and apply it to making a web application. Most of what I saw was pretty impressive, so it was cool to know that in that short period of time, I would have all that wealth of information. The downside is I am going to have to learn all that information.</p><p>Those feelings of &ldquo;fear&rdquo; and &ldquo;dread&rdquo; were conjured up thanks to those students that presented that evening. Most of them gave us new students pearls of wisdom like “It’s ok to cry” and “You’re going to wish you were dead”. I have a full-time job, a wife, two young children, and a mortgage, and I already feel dead most of the time. To be clear, when I say “dead”, I mean tired, not dead inside.</p><p>What the hell have I gotten myself into?</p><p>Sitting in my seat that night, I already had thoughts running through my head like &lquo;I bet it’s not too late to get a reimbursement on my tuition&rdquo;, and &lquo;I wonder if they would notice if I escaped through those windows&rdquo;. The worst part of it was that I couldn’t even really turn to a fellow classmate and say &ldquo;are you as scared as I am?&rdquo; since I wasn’t really familiar with any of them yet.</p><p>There was some light at the end of the tunnel, however. After the students were done presenting, our head instructor told us to try and not be too overwhelmed. She told us that, yes we’ll learn a lot of information in a short span of time, but we will be armed with numerous resources to aid us through the process. Resources such as group members that we can turn to, several instructors and their assistants, and plenty of opportunities to mess up and learn from our mistakes.</p>"
+};
+
+var post3 = {
+	postTitle: "Straight, No Chaser",
+	postDate: "July 25, 2017",
+	postBody: "<p>It’s the third week of class, and we’ve already managed to cover HTML, CSS, and just began JavaScript last night. If I have to assign my experience here thus far as a metaphor, it would be “straight, no chaser.” There was no soft opening to our JS lessons; no “ok, we’re just going to ease into this.” Simply, BANG! And we’re off to the races! I feel like I made the right decision to make friends, and make them early. A few of them came to my aid last night, when I inevitably found myself lost amongst a slew of JS code.</p><p>After class, I headed home to eat a late dinner. Afterward, I pulled my laptop back out, and I stared at the code/program that we wrote in class until it made sense. I went through it line by line until I had my own version of a “ah-ha” moment. It wasn’t like a lightbulb went on or anything; it was more like puzzle pieces slowing coming together, but the puzzle is still incomplete.</p><p>On a positive note, I finally finished my pre-work (the Treehouse FED track) the other day, so I have that going for me.</p><p>Tonight we’re going over objects in JavaScript, so I’m hoping that goes over well enough to where I’m not crying softly after class.</p><p>Signing off…</p>"
+};
+
+var post4 = {
+	postTitle: "JavaScript is Less Scary with Friends",
+	postDate: "July 27, 2017",
+	postBody: "<p>After only two lessons worth of JavaScript, I thought that my head was going to explode. I was thinking to myself &ldquo;Isn’t it too early in my NSS career to have a meltdown already?&rdquo; It’s becoming increasingly clearer to me that I’m going to have to put in some serious man-hours in order to be able to absorb and regurgitate this material. It’s time to pull out the big guns.</p><p>So for Wednesday of this week, I called out of work sick, and dedicated most of the day to going over what we learned in class the previous two days. I also had the opportunity to meet up with Dre from my group. I guess what they say about two heads being better than one is quite true. Together, we were able to help each other understand the material we just learned, and get a great jump on this week’s homework assignments. There were certain things that I had a hard time wrapping my head around, like for loops and using them to cycle through an array. But after my meetup with Dre, I feel like I have a much better understanding now. Not a complete understanding, but I definitely feel more confident today than I did yesterday and the two days prior.</p><p>It’s days like today where I truly believe I made the right choice in going to NSS. I feel like having the opportunity to have physical teachers to learn from (not just videos), as well as fellow classmates to consult with, is well worth the price of tuition.</p><p>Until next time&hellip;</p>"
+};
+
+var post5 = {
+	postTitle: "My Blog 5",
+	postDate: "July 30, 2017",
+	postBody: "<p>Bart, with $10,000 we'd be millionaires! We could buy all kinds of useful things like…love! Kids, you tried your best and you failed miserably. The lesson is, never try. Your guilty consciences may make you vote Democratic, but secretly you all yearn for a Republican president to lower taxes, brutalize criminals, and rule you like a king!</p><p>Please do not offer my god a peanut. Oh, a *sarcasm* detector. Oh, that's a *really* useful invention! Save me, Jeebus. I don't like being outdoors, Smithers. For one thing, there's too many fat children.</p><p>A woman is a lot like a refrigerator. Six feet tall, 300 pounds…it makes ice. Bart, with $10,000 we'd be millionaires! We could buy all kinds of useful things like…love! I didn't get rich by signing checks.</p><p>Our differences are only skin deep, but our sames go down to the bone. Duffman can't breathe! OH NO! I hope I didn't brain my damage. Hi. I'm Troy McClure. You may remember me from such self-help tapes as 'Smoke Yourself Thin' and 'Get Some Confidence, Stupid!'</p>"
+};
+
+// Push all the post variables into the empty array
+allPosts.push(post1);
+allPosts.push(post2);
+allPosts.push(post3);
+allPosts.push(post4);
+allPosts.push(post5);
+
+// Code to grab id DIV ID from the HTML
+var blogContainer = document.getElementById("blog-container");
+
+for (var i = 0; i < allPosts.length; i++) {
+
+	var currentPost = allPosts[i];
+
+	var domString = "";
+
+	domString += '<section class="blog-post">';
+    domString +=    '<div class="blog-title">';
+    domString +=      '<h4>' + currentPost.postTitle + '</h4>';
+    domString +=      '<p class="blog-date">' + currentPost.postDate + '</p>';
+    domString +=    '</div>';
+    domString +=    '<div class="blog-body">';
+	domString +=	  '<p>' + currentPost.postBody + '</p>';
+    domString +=    '</div>'
+    domString += '</section>'
+
+    blogContainer.innerHTML += domString;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
